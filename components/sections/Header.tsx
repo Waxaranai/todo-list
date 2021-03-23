c25aab60dd5dc957bae37fe955988f5fc551b9ed
Waxaranai <waxaranai@gmail.com>
2021-03-23 22:46:53 +0700

    feat: add theme toggler

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -1,55 +1,76 @@
 import React from "react";
 import MenuItem from "../ui/menu/MenuItem";
-import { Box, Flex } from "@chakra-ui/react";
-import { MenuOpenIcon, MenuCloseIcon } from "../ui/menu/MenuIcon";
+import { Box, Button, ButtonGroup, Flex, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { MoonIcon, SunIcon, HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { signIn } from "next-auth/client";
 
-interface HeaderState {
-    isOpen: boolean;
-}
-export default class Header extends React.Component<{}, HeaderState> {
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            isOpen: false
-        };
-    }
-    render() {
-        const toggleMenu = () => this.setState({ isOpen: !this.state.isOpen });
-        return (
-            <Flex
-                as="nav"
-                align="center"
-                justify="space-between"
-                wrap="wrap"
-                w="100%"
-                mb={8}
-                p={8}
-                bg={["primary.500", "primary.500", "transparent", "transparent"]}
-                color={["white", "white", "primary.700", "primary.700"]}
-                {...this.props}
-            >
-                <Flex align="center">
-                    {/* For an Logo component */}
-                </Flex>
-
-                <Box display={{ base: "block", md: "none" }} onClick={toggleMenu}>
-                    {this.state.isOpen ? <MenuCloseIcon /> : <MenuOpenIcon />}
-                </Box>
-
-                <Box
-                    display={{ base: this.state.isOpen ? "block" : "none", md: "block" }}
-                    flexBasis={{ base: "100%", md: "auto" }}
-                >
-                    <Flex
-                        align="center"
-                        justify={["center", "space-between", "flex-end", "flex-end"]}
-                        direction={["column", "row", "row", "row"]}
-                        pt={[4, 4, 0, 0]}
-                    >
-                        <MenuItem to="/">Home</MenuItem>
-                    </Flex>
-                </Box>
+export default function Header(props) {
+    const { colorMode, toggleColorMode } = useColorMode();
+    const navColor = useColorModeValue("transparent", "gray.900");
+    const iconColor = useColorModeValue("black", "white");
+    const themeIcon = colorMode === "light" ? <MoonIcon /> : <SunIcon />;
+    const [navShow, setNavOpen] = React.useState(false);
+    const toggleMenu = () => setNavOpen(!navShow);
+    return (
+        <Flex
+            as="nav"
+            align="center"
+            justify="space-between"
+            wrap="wrap"
+            w="100%"
+            mb={8}
+            p={3}
+            bg={navColor}
+            color="white"
+            {...props}
+        >
+            <Flex align="left">
+                {/* For an Logo component */}
             </Flex>
-        );
-    }
+
+            <Box display={{ base: "block", md: "none" }}>
+                <ButtonGroup isAttached color={iconColor}>
+                    <Button mr="-px" onClick={toggleColorMode}>
+                        {themeIcon}
+                    </Button>
+                    <Button onClick={toggleMenu}>
+                        {navShow ? <CloseIcon /> : <HamburgerIcon />}
+                    </Button>
+                </ButtonGroup>
+
+            </Box>
+
+            <Box
+                display={{ base: navShow ? "block" : "none", md: "block" }}
+                flexBasis={{ base: "100%", md: "auto" }}
+            >
+                <Flex
+                    align="center"
+                    justify={["center", "flex-end", "flex-end", "flex-end"]}
+                    direction={["column", "row", "row", "row"]}
+                    pt={[5, 5, 0, 0]}
+                >
+                    <Box display={{ base: "none", md: "block" }} >
+                        <ButtonGroup>
+                            <MenuItem to="/">
+                                <Button fontWeight="normal">Home</Button>
+                            </MenuItem>
+                            <MenuItem to="/api/auth/signin" onClick={signIn}>
+                                <Button fontWeight="normal">Sign In</Button>
+                            </MenuItem>
+                            <MenuItem isLast>
+                                <Button onClick={toggleColorMode}>{themeIcon}</Button>
+                            </MenuItem>
+                        </ButtonGroup>
+                    </Box>
+                    <Box display={{ base: "block", md: "none" }} >
+                        <MenuItem to="/">Home</MenuItem>
+                        <MenuItem to="/api/auth/signin" onClick={(e) => { e.preventDefault(); signIn(); }} isLast>
+                            Sign In
+                        </MenuItem>
+                    </Box>
+                </Flex>
+            </Box>
+        </Flex>
+    );
 }
--- components/ui/menu/MenuItem.tsx
+++ components/ui/menu/MenuItem.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 import { Text, useColorModeValue } from "@chakra-ui/react";
 
 export default function MenuItem({ children, isLast = false, to = "/", ...rest }) {
-    const color = useColorModeValue("black", "white");
+    const color = useColorModeValue("gray.900", "white");
     return (
         <Text
-            mb={{ base: isLast ? 0 : 8, sm: 0 }}
-            mr={{ base: 0, sm: isLast ? 0 : 8 }}
+            mb={{ base: isLast ? 3 : 5, sm: 0 }}
+            mr={{ base: 0, sm: isLast ? 0 : 1 }}
             display="block"
+            color={color}
             {...rest}
-            textColor={color}
         >
             <Link href={to}>{children}</Link>
         </Text>
