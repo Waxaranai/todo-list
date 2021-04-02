8bc4e2703f99a6e5be940618576b21c5527738f9
Waxaranai <32866617+Waxaranai@users.noreply.github.com>
2021-04-02 19:58:18 +0700

    feat: add todos (#2)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { Provider } from "next-auth/client";
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { store, persistor } from "../redux/store";
 import { NextComponentType } from "next";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import { Provider as NextProvider } from "next-auth/client";
+import { Provider as ReduxProvider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 interface AppProps {
   Component: NextComponentType;
@@ -11,14 +14,18 @@ export default class App extends React.Component<AppProps> {
   render() {
     return (
       <ChakraProvider theme={theme}>
-        <Provider
-          options={{
-            clientMaxAge: 0,
-            keepAlive: 0
-          }}
-          session={this.props.pageProps.session} >
-          <this.props.Component {...this.props.pageProps} />
-        </Provider>
+        <ReduxProvider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <NextProvider
+              options={{
+                clientMaxAge: 0,
+                keepAlive: 0
+              }}
+              session={this.props.pageProps.session} >
+              <this.props.Component {...this.props.pageProps} />
+            </NextProvider>
+          </PersistGate>
+        </ReduxProvider>
       </ChakraProvider>
     );
   }
