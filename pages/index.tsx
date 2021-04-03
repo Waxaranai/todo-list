486e4e5627f7d4b976d8ca14a71d6735bc200611
Waxaranai <waxaranai@gmail.com>
2021-04-04 06:58:41 +0700

    feat: add filter tabs

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import Layout from "../components/Layout";
 import TodoInput from "../components/ui/TodoInput";
 import TodoItem from "../components/ui/TodoItem";
-import { Text, Stack } from "@chakra-ui/react";
+import { Text, Stack, Tabs, TabList, Tab, TabPanels, TabPanel } from "@chakra-ui/react";
 import { ITodoItem } from "../interfaces";
 import { connect } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
+import { DeleteIcon } from "@chakra-ui/icons";
 
 interface HomeProps {
   items: ITodoItem[]
 }
 class Home extends React.Component<HomeProps> {
   render() {
+    const completedItems = this.props.items.filter(item => item.completed);
+    const uncompletedItems = this.props.items.filter(item => !item.completed);
+    const noItemsText = (text: string) => this.props.items.length ? text : "You have no tasks, would you like to add one?";
     return (
       <Layout>
         <Stack textAlign={"center"} mt={5} mb={10} spacing={5}>
@@ -21,12 +25,50 @@ class Home extends React.Component<HomeProps> {
           </Text>
           <TodoInput />
         </Stack>
-        <Stack mb={10} spacing={3.5}>
-          {this.props.items &&
-            (this.props.items.map(item =>
-              (<TodoItem key={uuidv4()} item={item} />)
-            ))
-          }
+        <Stack mb={10}>
+          <Tabs isFitted variant={"enclosed"} colorScheme={"teal"}>
+            <TabList>
+              <Tab>All</Tab>
+              <Tab>Completed</Tab>
+              <Tab>Uncompleted</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <Stack spacing={3.5}>
+                  {this.props.items.length ? this.props.items.map(item =>
+                    (<TodoItem key={uuidv4()} item={item} />)
+                  ) : (
+                    <Stack mt={20} textAlign={"center"}>
+                      <Text>You have no tasks, would you like to add one?</Text>
+                    </Stack>
+                  )}
+                </Stack>
+              </TabPanel>
+              <TabPanel>
+                <Stack spacing={3.5}>
+                  {completedItems.length ? completedItems.map(item =>
+                    (<TodoItem key={uuidv4()} item={item} />)
+                  ) : (
+                    <Stack mt={20} textAlign={"center"}>
+                      <Text>{noItemsText("You haven't completed any task!")}</Text>
+                    </Stack>
+                  )}
+                </Stack>
+              </TabPanel>
+              <TabPanel>
+                <Stack spacing={3.5}>
+                  {uncompletedItems.length ? uncompletedItems.map(item =>
+                    (<TodoItem key={uuidv4()} item={item} />)
+                  ) : (
+                    <Stack mt={20} textAlign={"center"}>
+                      <Text>{noItemsText("Congratulations, all tasks have been completed!")}</Text>
+                    </Stack>
+                  )}
+                </Stack>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+
         </Stack>
       </Layout>
     );
