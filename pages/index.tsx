c0a08eb860d808ddc7f9a0660af40abb2df89a4f
Waxaranai <32866617+Waxaranai@users.noreply.github.com>
2021-04-05 15:10:22 +0700

    feat(ItemList): using 1 component instead of repeating the code (#4)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Layout from "../components/Layout";
+import ItemList from "../components/ui/ItemList";
 import TodoInput from "../components/ui/TodoInput";
-import TodoItem from "../components/ui/TodoItem";
 import { Text, Stack, Tabs, TabList, Tab, TabPanels, TabPanel } from "@chakra-ui/react";
 import { ITodoItem } from "../interfaces";
 import { connect } from "react-redux";
-import { v4 as uuidv4 } from "uuid";
 
 interface HomeProps {
   items: ITodoItem[]
@@ -14,7 +13,7 @@ class Home extends React.Component<HomeProps> {
   render() {
     const completedItems = this.props.items.filter(item => item.completed);
     const uncompletedItems = this.props.items.filter(item => !item.completed);
-    const noItemsText = (text: string) => this.props.items.length ? text : "You have no tasks, would you like to add one?";
+    const handleText = (text: string) => this.props.items.length ? text : "You have no tasks, would you like to add one?";
     return (
       <Layout>
         <Stack textAlign={"center"} mt={5} mb={10} spacing={5}>
@@ -33,41 +32,16 @@ class Home extends React.Component<HomeProps> {
             </TabList>
             <TabPanels>
               <TabPanel>
-                <Stack spacing={3.5}>
-                  {this.props.items.length ? this.props.items.map(item =>
-                    (<TodoItem key={uuidv4()} item={item} />)
-                  ) : (
-                    <Stack mt={20} textAlign={"center"}>
-                      <Text>You have no tasks, would you like to add one?</Text>
-                    </Stack>
-                  )}
-                </Stack>
+                <ItemList data={this.props.items} text={handleText("You have no tasks, would you like to add one?")} />
               </TabPanel>
               <TabPanel>
-                <Stack spacing={3.5}>
-                  {completedItems.length ? completedItems.map(item =>
-                    (<TodoItem key={uuidv4()} item={item} />)
-                  ) : (
-                    <Stack mt={20} textAlign={"center"}>
-                      <Text>{noItemsText("You haven't completed any task!")}</Text>
-                    </Stack>
-                  )}
-                </Stack>
+                <ItemList data={completedItems} text={handleText("You haven't completed any task!")} />
               </TabPanel>
               <TabPanel>
-                <Stack spacing={3.5}>
-                  {uncompletedItems.length ? uncompletedItems.map(item =>
-                    (<TodoItem key={uuidv4()} item={item} />)
-                  ) : (
-                    <Stack mt={20} textAlign={"center"}>
-                      <Text>{noItemsText("Congratulations, all tasks have been completed!")}</Text>
-                    </Stack>
-                  )}
-                </Stack>
+                <ItemList data={uncompletedItems} text={handleText("Congratulations, all tasks have been completed!")} />
               </TabPanel>
             </TabPanels>
           </Tabs>
-
         </Stack>
       </Layout>
     );
